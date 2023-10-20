8058b143ae00cfc7e984508b885dcb5a063082e1
Pr Things <prthings@skiff.com>
2023-10-20 18:09:39 +0530

    added buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import {Link} from 'react-scroll'
 
 const Home = () => {
   return (
@@ -11,7 +12,7 @@ const Home = () => {
             <h2 className='text-6xl sm:text-7xl font-bold text-[#284339]'>I am Full Stack Developer</h2>
             <p className='py-4 max-w-[700px] text-[#f1f5f9]'>I'm a full stack developer specializing in building responsive design and developing full stack web applications. </p>
             <div>
-                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#284339] hover:border-[#284339] rounded'>View Projects <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-2' /></span></button>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#284339] hover:border-[#284339] rounded'><Link to='work'>View Projects</Link><span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-2' /></span></button>
             </div>
         </div>
     </div>
